a9e495224fc9aac1300ccd19e74be6cbc77dfef8
ispan <ispan@DESKTOP-PAKSETB>
2022-10-17 17:56:55 +0800

    加入專案檔案。

---
+++ 第三題/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第三題
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string sentence = ("我是第一行\n我是第二行");
+			Console.WriteLine(sentence);
+		}
+	}
+}
---
+++ 第二題/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第二題
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = ("我是一個\"好學生\"");
+			Console.WriteLine(name);
+		}
+	}
+}
